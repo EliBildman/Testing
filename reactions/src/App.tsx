2c8cf4454b21b194f97e8fd784efceaac7fd8f63
Eli Bildman <esbildman@gmail.com>
2020-06-12 23:07:13 -0400

    initial commit

---
+++ reactions/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Bar } from './iconBar';
+import './App.css';
+import { createMuiTheme } from '@material-ui/core';
+
+
+function App() {
+  return (
+    <Bar notifications={10} mail={5} avatarPath="me.jpg" />
+  );
+}
+
+export default App;
